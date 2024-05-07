3ccc9d92f69663469c08d21169ff3a2e6bbd8f1c
poovarasansivakumar2003 <132900038+poovarasansivakumar2003@users.noreply.github.com>
2024-05-07 07:33:03 +0530

    Update ArrayTree.C
    Fixing error

--- ArrayTree.C
+++ ArrayTree.C
@@ -16,12 +16,12 @@ void main(){
     for (i = 1; i <= 7; i++){
 	if (a[i] == 0)
 	    continue;
-	printf("\nIs there a left child node for %d node? YES 1 NO 0: ", a[i]);
+	left: printf("\nIs there a left child node for %d node? YES 1 NO 0: ", a[i]);
 	scanf("%d", &option);
 	if (option){
-	    left: printf("\nEnter left child node for %d node: ", a[i]);
+	    printf("\nEnter left child node for %d node: ", a[i]);
 	    scanf("%d",&a[2*i]);
-	    if (a[i]<a[2*i] || ((2*i)>4 && i%2!=0 && a[2*i]<a[i/2])) {
+	    if (a[i]<a[2*i] || ((2*i)>4 && (i%2)!=0 && a[2*i]<a[i/2])) {
 		printf("\nInvalid input, please try again.\n");
 		goto left;
 	    }
@@ -34,7 +34,7 @@ void main(){
 	if (option) {
 	    printf("\nEnter right child node for %d node: ", a[i]);
 	    scanf("%d",&a[2*i+1]);
-	    if (a[i] > a[2*i+1] || ((2*i+1)>4 && i%2==0 && a[2*i+1] > a[i/2])) {
+	    if (a[i] > a[2*i+1] || ((2*i+1)>4 && (i%2)==0 && a[2*i+1] > a[i/2])) {
 		printf("\nInvalid input, please try again.\n");
 		goto right;
 	    }
