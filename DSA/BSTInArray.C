24c9d3cd8f88312f7e7cd3174f389b64037ce71e
poovarasansivakumar2003 <132900038+poovarasansivakumar2003@users.noreply.github.com>
2024-05-13 15:42:18 +0530

    Update and rename ArrayTree.C to DSA/BSTInArray.C

--- DSA/BSTInArray.C
+++ DSA/BSTInArray.C
@@ -1,27 +1,32 @@
+/**********************************************************
+Representation of Binary Search tree in array
+***********************************************************/
+//Header files
 #include<stdio.h>
 #include<conio.h>
 
+//Main function
 void main(){
     static int a[16];
     int i, option;
     clrscr();
-    a[0] = 0;
+    a[0] = 0;//First element in array is zero
     printf("Tree Operation on arrays\n");
     printf("\nEnter the value of root node: \n");
     scanf("%d", &a[1]);
     if (a[1] == 0) {
-	printf("\nInvalid");
+	printf("\nInvalid");//Root element cannot be zero
 	return;
     }
-    for(i=1;i<=7;i++){
-	if (a[i] == 0)
+    for(i=1;i<=7;i++){//Number of child in level = height-2
+	if (a[i] == 0)//If parent is zero then child is zero
 	    continue;
 	left:printf("\nIs there a left child node for %d node? YES 1 NO 0: ", a[i]);
 	scanf("%d", &option);
 	if (option){
 	    printf("\nEnter left child node for %d node: ", a[i]);
 	    scanf("%d", &a[2*i]);
-	    if (a[i]<=a[2*i] || ((2*i)>4 && (i%2)!=0 && a[2*i]<a[i/2])) {
+	    if (a[i]<=a[2*i] || ((2*i)>4 && (i%2)!=0 && a[2*i]<a[i/2])) {//Invalid condition for satisfying BST
 		printf("\nInvalid input, please try again.\n");
 		goto left;
 	    }
@@ -34,7 +39,7 @@ void main(){
 	if (option) {
 	    printf("\nEnter right child node for %d node: ", a[i]);
 	    scanf("%d", &a[2*i+1]);
-	    if (a[i]>=a[2*i+1] || ((2*i+1)>4 && (i%2)==0 && a[2*i+1]>a[i/2])) {
+	    if (a[i]>=a[2*i+1] || ((2*i+1)>4 && (i%2)==0 && a[2*i+1]>a[i/2])) {//Invalid condition for satisfying BST
 		printf("\nInvalid input, please try again.\n");
 		goto right;
 	    }
@@ -44,7 +49,7 @@ void main(){
 	}
     }
 
-    printf("\nDo you want to display array? YES 1 NO 0: ");
+    printf("\nDo you want to display array? YES 1 NO 0: ");//Asking user to display or not
     scanf("%d", &option);
     if (option) {
 	for(i= 0;i<16;i++)
